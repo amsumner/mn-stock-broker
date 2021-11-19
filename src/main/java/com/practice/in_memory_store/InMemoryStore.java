915ee33bd58998e8269dc578b83f8b760e6ef8cc
amsumner <amsumner@deloitte.co.uk>
2021-11-19 00:38:47 +0000

    Added QuotesController and associated tests for path variables

--- src/main/java/com/practice/in_memory_store/InMemoryStore.java
+++ src/main/java/com/practice/in_memory_store/InMemoryStore.java
@@ -1,9 +1,15 @@
 package com.practice.in_memory_store;
 
+import com.practice.model.Quote;
 import com.practice.model.Symbol;
 import jakarta.inject.Singleton;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,15 +17,41 @@ import java.util.stream.Stream;
 public class InMemoryStore {
 
     private final List<Symbol> symbols;
+    private final ThreadLocalRandom current = ThreadLocalRandom.current();
+    private Map<String, Quote> cachedQuotes = new HashMap<>();
 
     public InMemoryStore() {
         symbols = Stream.of("AMZN","AAPL","FB","GOOG","MSFT","NFLX","TSLA")
                 .map(Symbol::new)
                 .collect(Collectors.toList());
+        symbols.forEach(symbol -> {
+            cachedQuotes.put(symbol.getValue(), randomQuote(symbol));
+        });
+    }
 
+    private Quote randomQuote(final Symbol symbol) {
+      return   Quote.builder()
+                .ask(randomValue())
+                .bid(randomValue())
+                .lastPrice(randomValue())
+                .symbol(symbol)
+                .volume(randomValue())
+                .build();
     }
 
     public List<Symbol> getAllSymbols() {
         return symbols;
     };
+
+    public Optional<Quote> fetchQuote(final String symbol) {
+        return Optional.ofNullable(cachedQuotes.get(symbol));
+    }
+
+    public BigDecimal randomValue() {
+        return BigDecimal.valueOf(current.nextDouble(1,100));
+    }
+
+    public void update(Quote quote) {
+    cachedQuotes.put(quote.getSymbol().getValue(), quote);
+    }
 }
---
+++ src/main/java/com/practice/model/Quote.java
@@ -0,0 +1,22 @@
+package com.practice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Quote {
+
+    private Symbol symbol;
+    private BigDecimal bid;
+    private BigDecimal ask;
+    private BigDecimal lastPrice;
+    private BigDecimal volume;
+}
