3fc33054ae68642618c3e3258568ebc2ad35eb13
amsumner <amsumner@deloitte.co.uk>
2021-11-24 20:16:53 +0000

    Added QuotesController and QuotesControllerTest and CustomError

---
+++ src/main/java/com/practice/model/error/CustomError.java
@@ -0,0 +1,21 @@
+package com.practice.model.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomError {
+
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+
+
+}
