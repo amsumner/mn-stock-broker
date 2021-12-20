2f259b87743b625e13969e6d06f02888e1bde006
amsumner <amsumner@deloitte.co.uk>
2021-12-20 14:36:52 +0000

    Added WatchListControllerRx, WatchListControllerTestRx and updating dependencies

---
+++ src/main/java/com/practice/controller/WatchListControllerRx.java
@@ -0,0 +1,61 @@
+package com.practice.controller;
+
+import com.practice.in_memory_store.InMemoryAccountStore;
+import com.practice.model.WatchList;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import jakarta.inject.Named;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+
+@Slf4j
+@Controller("/account/watchlist-rx")
+public class WatchListControllerRx {
+
+    static final UUID ACCOUNT_ID = UUID.randomUUID();
+
+    private final InMemoryAccountStore store;
+
+    private final Scheduler scheduler;
+
+    public WatchListControllerRx(@Named(TaskExecutors.IO) ExecutorService executorService,
+                                 InMemoryAccountStore store) {
+        this.store = store;
+        this.scheduler = Schedulers.from(executorService);
+    }
+
+    @Get(produces = MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
+    public WatchList get() {
+        log.info("GetWatchList - {}", Thread.currentThread().getName());
+        return store.getWatchList(ACCOUNT_ID);
+    }
+
+    @Get(value = "/single", produces = MediaType.APPLICATION_JSON)
+    public @NonNull Flowable<WatchList> getReactive() {
+        log.info("getAsSingle - {}", Thread.currentThread().getName());
+        return Single.fromCallable(() -> store.getWatchList(ACCOUNT_ID)).toFlowable().subscribeOn(scheduler);
+    }
+
+    @Put(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
+    public WatchList update(@Body WatchList watchList) {
+        return store.updateWatchList(ACCOUNT_ID, watchList);
+    }
+
+    @Delete(value = "/{accountId}")
+    @ExecuteOn(TaskExecutors.IO)
+    public void delete(@PathVariable UUID accountId) {
+        store.deleteWatchList(accountId);
+    }
+
+}
---
+++ src/test/java/com/practice/controller/WatchListControllerTestRx.java
@@ -0,0 +1,123 @@
+package com.practice.controller;
+
+import com.practice.in_memory_store.InMemoryAccountStore;
+import com.practice.model.Symbol;
+import com.practice.model.WatchList;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.rxjava3.http.client.Rx3HttpClient;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@MicronautTest
+public class WatchListControllerTestRx {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WatchListControllerTestRx.class);
+    private static final UUID ACCOUNT_ID = WatchListControllerRx.ACCOUNT_ID;
+
+    @Inject
+    EmbeddedApplication application;
+
+    //TODO: Find why i cannot use RxHttpClient
+    @Inject
+    @Client("/account")
+    Rx3HttpClient client;
+
+    @Inject
+    InMemoryAccountStore store;
+
+    public BigDecimal randomValue() {
+        return BigDecimal.valueOf(ThreadLocalRandom.current().nextDouble(1,100));
+    }
+
+    @Test
+    void returnsEmptyWatchListForAccount() {
+        final WatchList result = client.toBlocking()
+                .retrieve(HttpRequest.GET("/watchlist-rx"), Argument.of(WatchList.class));
+    assertTrue(result.getSymbols().isEmpty());
+    assertTrue(store.getWatchList(ACCOUNT_ID).getSymbols().isEmpty());
+    }
+
+    @Test
+    void returnWatchListForAccount() {
+        final List<Symbol> symbols = Stream.of("AMZN", "NFLX", "AAPL")
+                .map(Symbol::new)
+                .collect(Collectors.toList());
+        WatchList watchList = new WatchList(symbols);
+        store.updateWatchList(ACCOUNT_ID, watchList);
+
+        final WatchList result = client.toBlocking()
+                .retrieve(HttpRequest.GET("/watchlist-rx"), WatchList.class);
+        assertEquals(3 , result.getSymbols().size());
+        assertEquals(3, store.getWatchList(ACCOUNT_ID).getSymbols().size());
+    }
+
+    @Test
+    void returnWatchListForAccountAsSingle() {
+        final List<Symbol> symbols = Stream.of("AMZN", "NFLX", "AAPL")
+                .map(Symbol::new)
+                .collect(Collectors.toList());
+        WatchList watchList = new WatchList(symbols);
+        store.updateWatchList(ACCOUNT_ID, watchList);
+
+        final WatchList result = client.toBlocking()
+                .retrieve(HttpRequest.GET("/watchlist-rx/single"), WatchList.class);
+        assertEquals(3 , result.getSymbols().size());
+        assertEquals(3, store.getWatchList(ACCOUNT_ID).getSymbols().size());
+    }
+
+    @Test
+    void canUpdateWatchListForAccount() {
+        final List<Symbol> symbols = Stream.of("AMZN", "NFLX", "AAPL")
+                .map(Symbol::new)
+                .collect(Collectors.toList());
+        WatchList watchList = new WatchList(symbols);
+
+        final HttpResponse<WatchList> result = client.toBlocking()
+                .exchange(HttpRequest.PUT("/watchlist-rx", watchList), WatchList.class);
+        assertEquals(HttpResponse.ok().status(), result.getStatus());
+        assertEquals(3 , result.body().getSymbols().size());
+        assertEquals(watchList, store.getWatchList(ACCOUNT_ID));
+    }
+
+    @Test void canDelteWatchlistForAccount() {
+        final List<Symbol> symbols = Stream.of("AMZN", "NFLX", "AAPL")
+                .map(Symbol::new)
+                .collect(Collectors.toList());
+        WatchList watchList = new WatchList(symbols);
+
+        store.updateWatchList(ACCOUNT_ID ,watchList);
+
+        assertEquals(3, store.getWatchList(ACCOUNT_ID).getSymbols().size());
+
+        final HttpResponse response = client.toBlocking()
+                .exchange(HttpRequest.DELETE("/watchlist-rx/" + ACCOUNT_ID));
+
+        assertTrue(store.getWatchList(ACCOUNT_ID).getSymbols().isEmpty());
+
+
+
+    }
+
+
+}
